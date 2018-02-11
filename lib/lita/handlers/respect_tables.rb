b85ff9044f57a2f3250faa0c0aa07cd9bf46e0a2
gouf <innocent.zero@gmail.com>
2018-02-12 05:27:46 +0900

    Implement the respect tables

--- lib/lita/handlers/respect_tables.rb
+++ lib/lita/handlers/respect_tables.rb
@@ -1,7 +1,12 @@
 module Lita
   module Handlers
     class RespectTables < Handler
-      # insert handler code here
+      # Expected "chabu-dai-gaeshi" like: (ノ｀Д´)ノ彡┻━┻
+      route(/┻━┻/, :respect_tables)
+
+      def respect_tables(response)
+        response.reply('┬─┬ノ(ಠ_ಠノ)')
+      end
 
       Lita.register_handler(self)
     end
